4461bf310bd9bc10b59a62c6b5be54a4750e9377
gihwanJang <90659997+gihwanJang@users.noreply.github.com>
2024-06-17 20:52:48 +0900

    fix : client

--- src/main/java/com/nhnacademy/gateway2/Config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/gateway2/Config/RouteLocatorConfig.java
@@ -1,9 +1,12 @@
 package com.nhnacademy.gateway2.Config;
 
 import com.nhnacademy.gateway2.filter.JwtAuthorizationHeaderFilter;
+import feign.codec.Encoder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
@@ -19,15 +22,17 @@ public class RouteLocatorConfig {
 
         //TODO#1-3 서비스 명과 path는 추후 수정 됩니다.
         return builder.routes()
-                .route("auth-service", p->p.path("/login")
-                        //TODO#1-3 jwt를 검증할 Filter를 등록합니다
-                        .uri("lb://AUTH-SERVICE")
-                )
-                //TODO#1-2 order 서비스는 jwtAuthorizationHeaderFilter가 적용됩니다. (인증 없이 접근 시 로그인 페이지로 이동)
-                .route("order-service", p->p.path("/order")
-                        .filters(f->f.filter(jwtAuthorizationHeaderFilter.apply(new JwtAuthorizationHeaderFilter.Config())))
-                        .uri(("lb://ORDER-SERVICE")
-                        ))
+//                .route("auth-service", p->p.path("/login")
+//                        //TODO#1-3 jwt를 검증할 Filter를 등록합니다
+//                        .uri("lb://AUTH-SERVICE")
+//                )
+//                //TODO#1-2 order 서비스는 jwtAuthorizationHeaderFilter가 적용됩니다. (인증 없이 접근 시 로그인 페이지로 이동)
+//                .route("order-service", p->p.path("/order")
+//                        .filters(f->f.filter(jwtAuthorizationHeaderFilter.apply(new JwtAuthorizationHeaderFilter.Config())))
+//                        .uri(("lb://ORDER-SERVICE")
+//                        ))
+                .route("client", p -> p.path("/api/client/**")
+                        .uri("lb://CLIENT"))
                 .build();
 
     }
@@ -35,6 +40,16 @@ public class RouteLocatorConfig {
     @Bean
     public ServerCodecConfigurer serverCodecConfigurer() {
         return ServerCodecConfigurer.create();
+    }
+
+    @Bean
+    public HttpMessageConverters httpMessageConverters() {
+        return new HttpMessageConverters();
+    }
+
+    @Bean
+    public Encoder feignEncoder(HttpMessageConverters messageConverters) {
+        return new SpringEncoder(() -> messageConverters);
     }
 
 }
